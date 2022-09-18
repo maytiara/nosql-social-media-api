fe193bd4dc3e50b9c65cff738890f36c2c6c1553
May Carandang <maytiara@gmail.com>
2022-09-19 04:09:57 +0800

    Revised server setup

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
-require('./config/connection'); // MongoDB connection
+const db = require('./config/connection'); // MongoDB connection
 const express = require('express'); // express.js
 //const routes = require('./routes'); // API endpoints
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = 3001;
 
 // Express middleware
 app.use(express.json());
@@ -13,6 +13,8 @@ app.use(express.urlencoded({ extended: true })); // to Parse URL data from any f
 //app.use(routes);
 
 // Server setup
-app.listen(PORT, () => {
-  console.log(`Server listening on ${PORT} 🚀`)
+db.once('open', () => {
+  app.listen(PORT, () => {
+    console.log(`API Server listening on ${PORT} 🚀`);
+  });
 });
