0b57ec3583711399fac397e751a091aa790f9e3c
May Carandang <maytiara@gmail.com>
2022-09-20 00:03:50 +0800

    added process.env.PORT

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express'); // express.js
 const routes = require('./routes/api'); // API endpoints
 
 const app = express();
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 // Express middleware
 app.use(express.json());
