29be33c20732cfb4497c83be4f17984c284353d2
May Carandang <maytiara@gmail.com>
2022-09-20 00:03:24 +0800

    Removed dotenv using standalone mongoose connection

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,10 @@
-require('dotenv').config();
 const { connect, connection } = require('mongoose');
 
 // Wrap Mongoose around local connection to MongoDB
 // Throwing an error using promise wrapper
 
 // For future reference: Add MONGODB_URI to connect // Atlas (same dotenv guide)
-connect(process.env.MONGODB_URL, {
+connect('mongodb://localhost/socialMedia_Module', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 }).then(() => {
