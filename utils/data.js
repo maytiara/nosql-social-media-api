bb08afca221595afa3b1d2b5a5f1ec3a76d3d313
May Carandang <maytiara@gmail.com>
2022-09-21 20:51:43 +0800

    Revised data structures === to model properties

--- utils/data.js
+++ utils/data.js
@@ -21,45 +21,70 @@ const seedUsers = [
   },
 ];
 
-const seedReactions = [
-  {
-    reactionBody: 'Cool! There are many variations'
-  },
-  {
-    reactionBody: 'Awesome! is simply dummy text'
-  },
-  {
-    reactionBody: 'Great! Contrary to popular belief'
-  },
-  {
-    reactionBody: 'Interesting! Various versions have evolved'
-  },
-  {
-    reactionBody: 'Unbelievable! combined with a handful of model sentence structures'
-  },
-];
-
 const seedThoughts = [
   {
-    thoughtText: 'Lorem Ipsum is not simply random text'
+    username: 'AParker',
+    thoughtText: 'Lorem Ipsum is not simply random text',
+    reactions: [
+      {
+        username: 'AParker',
+        reactionBody: 'Cool! There are many variations'
+      },
+    ]
   },
   {
-    thoughtText: 'Content here, content here'
+    username: 'Aaran',
+    thoughtText: 'Content here, content here',
+    reactions: [
+      {
+        username: 'Aaran',
+        reactionBody: 'Awesome! is simply dummy text'
+      },
+    ]
   },
   {
-    thoughtText: 'Scrambled it to make a type specimen book'
+    username: 'Zubair',
+    thoughtText: 'Scrambled it to make a type specimen book',
+    reactions: [
+      {
+        username: 'Aaran',
+        reactionBody: 'Great! Contrary to popular belief'
+      },
+    ]
   },
   {
-    thoughtText: 'You are going to use a passage of Lorem Ipsum'
+    username: 'Zubair',
+    thoughtText: 'You are going to use a passage of Lorem Ipsum',
+    reactions: [
+      {
+        username: 'Aaran',
+        reactionBody: 'Interesting! Various versions have evolved'
+      },
+    ]
   },
   {
-    thoughtText: 'Going through the cites of the word'
+    username: 'Zubair',
+    thoughtText: 'Going through the cites of the word',
+    reactions: [
+      {
+        username: 'Aaran',
+        reactionBody: 'Unbelievable! combined with a handful of model sentence structures'
+      },
+    ]
   },
 ]
 
 // Export the functions for use in seed.js
 module.exports = {
   seedUsers,
-  seedReactions,
   seedThoughts,
 };
+
+// seedUsers.map(async (p, index) => {
+//   await p.save((err, result) => {
+//     if (index === seedUsers.length -1) {
+//       console.log('Seeding complete! 🌱');
+//       connection.disconnect();
+//     }
+//   })
+// })
