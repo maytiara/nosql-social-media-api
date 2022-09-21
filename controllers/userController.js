7cd5390b0b34ac929c51faaf07b83a1d2e74d417
May Carandang <maytiara@gmail.com>
2022-09-22 02:29:04 +0800

    Defined the functionality to create new user

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,24 +1,31 @@
 const User = require("../models/User");
 
 module.exports = {
-	//  getUsers || GET
+	//  get all users
 	getUsers(req, res) {
 		User.find({})
-			.populate({ path: 'thoughts', select: '-__v'}) //fieldname &  __v: (versionKey) || the internal revision of the document
-			.populate({ path: 'friends', select: '-__v'}) //fieldname
+			.populate({ path: "thoughts", select: "-__v" }) //fieldname &  __v: (versionKey) || the internal revision of the document
+			.populate({ path: "friends", select: "-__v" }) //fieldname
 			.then((users) => {
 				res.json(users);
 			});
 	},
-	// getSingleUser || GET
+	// get a single user by _id
 	getSingleUser(req, res) {
 		User.findOne({ users: req.params.userId }) // the requested route parameter, calling the collection ID
-    .populate({ path: 'thoughts', select: '-__v'}) 
-    .populate({ path: 'friends', select: '-__v'}) 
+			.populate({ path: "thoughts", select: "-__v" })
+			.populate({ path: "friends", select: "-__v" })
 			.then((users) => {
 				res.json(users);
 			});
 	},
+  // create a new user
+  createUser(req, res) {
+    User.create(req.body)
+    .then((dbUserData) => {
+      res.json(dbUserData)
+    });
+  }
 };
 
 //  getUsers,
