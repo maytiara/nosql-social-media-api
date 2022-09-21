81d8a758ae9d47372eb3d65fdd4d20341b9a548e
May Carandang <maytiara@gmail.com>
2022-09-21 23:52:05 +0800

    Allocated diff User routes for specific endpoints

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -7,19 +7,25 @@ const {
   deleteUser,
   createFriend,
   removeFriend
-} = require('../../controllers/userController');
+} = require('../controllers/userController');
 
 // /api/users
 router.route('/')
       .get(getSingleUser)
+      .post(createUser);
+
+// /api/:users/:userId
+router.route('/:userId')
       .get(getUsers)
-      .post(createUser)
       .put(updateUser)
       .delete(deleteUser);
 
+// /api/users/:userId/friends
+router.route('/:userId/friends')
+      .post(createFriend);
+
 // /api/users/:userId/friends/:friendId
 router.route('/:userId/friends/:friendId')
-      .post(createFriend)
       .delete(removeFriend);
 
 module.exports = router;
