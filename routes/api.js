5f2f7a25d2e8fdc1fb78baf85f8e71ec24ba79c4
May Carandang <maytiara@gmail.com>
2022-09-21 22:47:30 +0800

    Added pseudo code to define the actions

--- routes/api.js
+++ routes/api.js
@@ -2,7 +2,10 @@ const router = require('express').Router();
 const userRoutes = require('./postRoutes');
 const thoughtRoutes = require('./tagRoutes');
 
+// this will include friends
 router.use('/api/users', userRoutes);
+
+// this include reactions
 router.use('/api/thoughts', thoughtRoutes);
 
 module.exports = router;
