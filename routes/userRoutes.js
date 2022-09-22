e529cf99e4b8bd6045b05c89fcb915632e2f7979
May Carandang <maytiara@gmail.com>
2022-09-23 03:32:00 +0800

    Updated API routes with working functionalities

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -12,20 +12,17 @@ const {
 // /api/users
 router.route('/')
       .get(getUsers)
-//      .post(createUser);
+      .post(createUser);
 
 // /api/users/:userId
 router.route('/:userId')
-//      .get(getSingleUsers);
-//      .put(updateUser)
-//      .delete(deleteUser);
-//
-// /api/users/:userId/friends
-//router.route('/:userId/friends')
-//      .post(createFriend);
-//
-//// /api/users/:userId/friends/:friendId
-//router.route('/:userId/friends/:friendId')
-//      .delete(removeFriend);
+      .get(getSingleUser)
+      .put(updateUser)
+      .delete(deleteUser);
+
+// /api/users/:userId/friends/:friendId
+router.route('/:userId/friends/:friendId')
+//    .post(createFriend)
+//    .delete(removeFriend);
 
 module.exports = router;
