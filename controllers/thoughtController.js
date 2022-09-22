5a6fda0a08273b72e1b4aab7d164890bdf86dab8
May Carandang <maytiara@gmail.com>
2022-09-23 05:49:18 +0800

    Defined the functionalities of a new thoughts with new/existing user. This was also saved in User api routes

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -1,4 +1,5 @@
 const Thought = require('../models/Thought');
+const User = require('../models/User');
 
 module.exports = {
 	//  get all thoughts
@@ -12,9 +13,24 @@ module.exports = {
 	getSingleThought(req, res) {
 		// the requested route parameter, calling the collection ID
 		Thought.findOne({ _id: req.params.thoughtsId }) //finding the thoughts:ObjectId
-      .select('-__v') //fieldname & __v: (versionKey) || the internal revision of the document
+			.select("-__v") //fieldname & __v: (versionKey) || the internal revision of the document
+			.then((dbThoughtdata) => {
+				res.json(dbThoughtdata);
+			});
+	},
+
+	// create a new thought & push the created thought using _id nested to thoughts array
+	addThought(req, res) {
+		Thought.create(req.body)
+			.then((thoughts) => {
+				return User.findOneAndUpdate( // this push the data of thoughts in users collection
+					{ _id: req.body.userId },
+					{ $addToSet: { thoughts: thoughts._id } }, // this operator replaces the value of the field
+					{ upsert: true, new: true } // it updated the new insert value
+				);
+			})
 			.then((thoughts) => {
 				res.json(thoughts);
 			});
 	},
-}
+};
