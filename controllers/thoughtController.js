438ab35ff0d27998de886e5447edf4394294d94a
May Carandang <maytiara@gmail.com>
2022-09-23 05:25:26 +0800

    Defined functionality to get http request for a single thought using the ObjectId

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -7,4 +7,14 @@ module.exports = {
 			res.json(thoughts);
 		});
 	},
+
+	// get a single thought by _id /api/thoughts/ObjectId
+	getSingleThought(req, res) {
+		// the requested route parameter, calling the collection ID
+		Thought.findOne({ _id: req.params.thoughtsId }) //finding the thoughts:ObjectId
+      .select('-__v') //fieldname & __v: (versionKey) || the internal revision of the document
+			.then((thoughts) => {
+				res.json(thoughts);
+			});
+	},
 }
