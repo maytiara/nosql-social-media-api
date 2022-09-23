926da48012716a682014125a8df99ae901861e91
May Carandang <maytiara@gmail.com>
2022-09-23 16:50:32 +0800

    Added pseudocode to define parameter filter

--- controllers/userController.js
+++ controllers/userController.js
@@ -26,7 +26,7 @@ module.exports = {
 	// update a user by _id
 	updateUser(req, res) {
 		User.findOneAndUpdate(
-			{ _id: req.params.usersId },
+			{ _id: req.params.usersId }, //filter
 			{ $addToSet: { users: req.body } }, // this operator replaces the value of the field
 			{ upsert: true, new: true } // it updated the new insert value
 		).then((users) => {
@@ -36,7 +36,7 @@ module.exports = {
 	// remove user by _id
 	deleteUser(req, res) {
 		User.findOneAndDelete(
-			{ users: req.params.usersId },
+			{ users: req.params.usersId }, //filter
 			{ $pull: { users: req.params.usersId } } // this $pull operator deletes the value of the field
 		).then((users) => {
 			res.json(users);
