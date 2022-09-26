f307824337c863ae4f0f18c7bbbd13acae8315db
May Carandang <maytiara@gmail.com>
2022-09-27 02:40:19 +0800

    Changed the boolean property

--- controllers/userController.js
+++ controllers/userController.js
@@ -12,6 +12,7 @@ module.exports = {
 				res.json(users);
 			});
 	},
+	
 	// get a single user by _id /api/users/ObjectId
 	getSingleUser(req, res) {
 		// the requested route parameter, calling the collection ID
@@ -23,18 +24,21 @@ module.exports = {
 				res.json(users);
 			});
 	},
+
 	// create a new user
 	createUser(req, res) {
 		User.create(req.body).then((dbUserData) => {
 			res.json(dbUserData);
 		});
 	},
+
 	// update a user by _id
+	// returns null //--
 	updateUser(req, res) {
 		User.findOneAndUpdate(
 			{ _id: req.params.usersId }, //filter
 			{ $addToSet: { users: req.body } }, // this operator replaces the value of the field
-			{ upsert: true, new: true } // it updated the new insert value
+			{ runValidators: true, new: true } // it updated the new insert value
 		).then((users) => {
 			res.json(users);
 		});
@@ -42,16 +46,16 @@ module.exports = {
 
 	// remove user by _id
 	deleteUser(req, res) {
-		User.findOneAndDelete(
-			{ users: req.body.usersId }) //filter
+		User.findOneAndDelete({ users: req.body.usersId }) //filter
 			.then((users) => {
 				if (!users) {
 					res.status(404).json({ message: "ID not found! Please try again!" }); // it will returns 404
-			} else if (users) {
-				Thought.deleteMany ({_id: {$in: users.thoughts}})
+				} else if (users) {
+					Thought.deleteMany({ _id: { $in: users.thoughts } });
 				}
-			}).then(() => {
-				res.json({ message: 'Good Job! No turning back' });
+			})
+			.then(() => {
+				res.json({ message: "Good Job! No turning back" });
 			});
 	},
 
@@ -60,8 +64,8 @@ module.exports = {
 	createFriend(req, res) {
 		User.findOneAndUpdate(
 			{ _id: req.params.usersId },
-			{ $push: { path: "friends", $addToSet: { friends: req.body } } },
-			{ new: true }
+			{ $addToSet: { path: "friends", $push: { friends: req.body } } },
+			{ runValidators: true, new: true }
 		).then((users) => {
 			res.json(users);
 		});
